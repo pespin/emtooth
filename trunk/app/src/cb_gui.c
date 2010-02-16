7e028e48aa1ba9d408b1ded0061068a403fdf077
pespin.shar <pespin.shar@500b7156-e12a-11de-8d18-5b59c22968bc>
2010-02-16 17:18:34 +0000

    Don't remove paired devices when dissapear

--- trunk/app/src/cb_gui.c
+++ trunk/app/src/cb_gui.c
@@ -184,7 +184,6 @@ void cb_remove_device_clicked(void *data, Evas_Object *obj, void *event_info) {
 		 
 	bluez_adapter_remove_remote_device(device);
 	gui_device_list_remove(device);
-	/* TODO: free device */
 
 }
 
